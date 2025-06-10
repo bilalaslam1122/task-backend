8d80d93c6ba0dbae226c669d1f8b0979e308f832
bilalaslam1122 <bilalaslam6903@gmail.com>
2025-06-10 22:35:25 +0500

    update + delete endpoints

--- src/task/task.resolver.ts
+++ src/task/task.resolver.ts
@@ -2,6 +2,7 @@ import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { TaskService } from './task.service';
 import { Task } from './task.entity';
 import { CreateTaskInput } from './dto/create-task.input';
+import { UpdateTaskInput } from './dto/update-task.input';
 
 @Resolver(() => Task)
 export class TaskResolver {
@@ -20,5 +21,18 @@ export class TaskResolver {
   @Mutation(() => Task)
   createTask(@Args('input') input: CreateTaskInput) {
     return this.taskService.create(input);
+  }
+
+  @Mutation(() => Task)
+  updateTask(
+    @Args('id', { type: () => Int }) id: number,
+    @Args('input') input: UpdateTaskInput,
+  ) {
+    return this.taskService.update(id, input);
+  }
+
+  @Mutation(() => Boolean)
+  deleteTask(@Args('id', { type: () => Int }) id: number) {
+    return this.taskService.delete(id);
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Task } from './task.entity';
 import { CreateTaskInput } from './dto/create-task.input';
+import { UpdateTaskInput } from './dto/update-task.input';
 
 @Injectable()
 export class TaskService {
@@ -22,5 +23,18 @@ export class TaskService {
   async create(input: CreateTaskInput): Promise<Task> {
     const task = this.taskRepository.create(input);
     return this.taskRepository.save(task);
+  }
+
+  async update(id: number, input: UpdateTaskInput): Promise<Task> {
+    const task = await this.taskRepository.preload({ id, ...input });
+    if (!task) {
+      throw new NotFoundException(`Task #${id} not found`);
+    }
+    return this.taskRepository.save(task);
+  }
+
+  async delete(id: number): Promise<boolean> {
+    const result = await this.taskRepository.delete(id);
+    return (result.affected ?? 0) > 0;
   }
 }
